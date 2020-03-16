2fa74d766904f1861546dc555d316475b75fcf85
Ahmed El-Naggar <n10535594@qut.edu.au>
2020-03-16 15:43:55 +1000

    Created Project Structure
    *Based on the requirements released, the following packages were added:
    - server
    - control panel
    - viewer
    
    * .gitignore modified to ignore "out" folder

---
+++ src/controlpanel/Main.java
@@ -0,0 +1,9 @@
+package controlpanel;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+
+    }
+}
---
+++ src/viewer/Main.java
@@ -0,0 +1,9 @@
+package viewer;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+
+    }
+}
