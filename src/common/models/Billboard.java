17c06043d089fc12a6395b82cfce701590c13a8c
DIVYANSH TRIPATHI <divyanshtripathi1998@gmail.com>
2020-03-25 18:26:20 +0530

    User model created

--- src/common/models/Billboard.java
+++ src/common/models/Billboard.java
@@ -1,8 +1,11 @@
 package common.models;
 
 public class Billboard {
+    public String b_id;
     public String message;
     public String information;
     public String imageString;
-    public User createdBy;
+    public String userId;
+    public Boolean isUrl;
+    public String schedule_id;
 }
--- src/common/models/User.java
+++ src/common/models/User.java
@@ -1,7 +1,44 @@
 package common.models;
 
+import java.util.Arrays;
+
 public class User {
     public String name;
-    public String hashedPassword;
+    public String u_id;
     public String[] permissions;
+
+    User(String name,String[] permissions,String u_id){
+            this.u_id = u_id;
+            this.name = name;
+            this.permissions = permissions;
+    }
+
+    public void getUserByName(String name){
+        //Query the user table by name and create a user object
+
+        // sample data from server
+        String U_ID_fromServer = "123";
+        String[] permissions_fromServer = {"create_bb","edit_all_bb","schedule_bb","edit_user"};
+
+        this.permissions = permissions_fromServer;
+        this.u_id = U_ID_fromServer;
+        this.name = name;
+    }
+
+    public void getUserById(String u_id){
+        //Query the user table by name and create a user object
+
+        // sample data from server
+        String name_fromServer = "Zeegaroot";
+        String[] permissions_fromServer = {"create_bb","edit_all_bb","schedule_bb","edit_user"};
+
+        this.permissions = permissions_fromServer;
+        this.name = name_fromServer;
+        this.u_id = u_id;
+    }
+
+    public Boolean isAllowedTo(String permissionName){
+        return Arrays.asList(this.permissions).contains(permissionName);
+    }
+
 }
